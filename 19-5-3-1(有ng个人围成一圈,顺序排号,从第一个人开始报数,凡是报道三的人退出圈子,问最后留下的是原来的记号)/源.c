b9847e796e90fb90ab80457e71dff59fb917a7d4
YYaiHH <1903870154@qq.com>
2019-05-03 15:02:38 +0800

    提交代码

--- 19-5-3-1(有ng个人围成一圈,顺序排号,从第一个人开始报数,凡是报道三的人退出圈子,问最后留下的是原来的记号)/源.c
+++ 19-5-3-1(有ng个人围成一圈,顺序排号,从第一个人开始报数,凡是报道三的人退出圈子,问最后留下的是原来的记号)/源.c
@@ -4,25 +4,36 @@
 void fun(int n) {
 	int m = n;
 	int* p = malloc(n * sizeof(int));
+	for (int i = 0; i < n; ++i) {
+		*(p + i) = i + 1;
+		printf("%d\t", *(p + i));
+	}
+	printf("\n报数:\n");
 	for (int i = 0, k = 1;m > 1; ++i) {
 		if (*(p + i) != 0) {
-			*(p + i) = k;
+			printf("%d\t", k);
 			k += 1;
 			if (k == 4) {
 				k = 1;
+				*(p + i) = 0;
+				m -= 1;
 			}
 		}
-		if (*(p + i) == 3) {
-			*(p + i) = 0;
-			m -= 1;
-		}
+	
 		if (i == n - 1) {
+			printf("\n报到3的退出\n");
+			for (int j = 0; j < n; ++j) {
+				if (*(p + j) != 0) {
+					printf("%d\t", *(p + j));
+				}
+			}
+			printf("\n报数:\n");
 			i = -1;
 		}
 	}
 	for (int i = 0; i < n; ++i) {
 		if (*(p + i) != 0) {
-			printf("最后留下的是原来的%d号\n", i + 1);
+			printf("\n最后留下的是原来的%d号\n", *(p + i));
 		}
 	}
 }
