4fc24d8d5e9ddadf950179905d681972b81464d7
YYaiHH <1903870154@qq.com>
2019-05-09 21:02:42 +0800

    提交代码

---
+++ 19-5-9-3(在上一题的基础上,编写一个input函数,用来输入5)/19-5-9-3(在上一题的基础上,编写一个input函数,用来输入5)/源.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define N 5
+struct student {
+	int num;
+	char name[1024];
+	int score[3];
+};
+typedef struct student stu;
+void input(stu* p) {
+	for (int i = 0; i < N; ++i) {
+		printf("请输入num\n");
+		scanf("%d", &(p + i)->num);
+		printf("请输入姓名\n");
+		scanf("%s", &(p + i)->name);
+		for (int j = 0; j < 3; ++j) {
+			printf("请输入score%d\n", j + 1);
+			scanf("%d", &(p + i)->score[j]);
+		}
+	}
+}
+void print(stu* p) {
+	for (int i = 0; i < N; ++i) {
+		printf("num:%d\n", (p + i)->num);
+		printf("name:%s\n", (p + i)->name);
+		for (int j = 0; j < 3; ++j) {
+			printf("score%d: %d\n", j + 1, (p + i)->score[j]);
+		}
+	}
+}
+void main() {
+	stu arr[N];
+	stu* p = arr;
+	//输入
+	input(p);
+	//输出
+	print(p);
+	system("pause");
+}
