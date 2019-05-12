5f19baeb6d98314c30eba1cb8bb22eb11d6c520f
YYaiHH <1903870154@qq.com>
2019-05-12 15:16:41 +0800

    提交代码

--- 19-5-7-1(写一个函数,实现两个字符串的比较Strcmp)/19-5-7-1(写一个函数,实现两个字符串的比较Strcmp)/源.c
+++ 19-5-7-1(写一个函数,实现两个字符串的比较Strcmp)/19-5-7-1(写一个函数,实现两个字符串的比较Strcmp)/源.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define N 1024
 int Strcmp(char* p1, char* p2) {
-	for (int i = 0; i < 1023; ++i) {
+	for (int i = 0; i < N; ++i) {
 		if (p1[i] != p2[i]) {
 			return p1[i] - p2[i];
 		}
@@ -12,21 +13,23 @@ int Strcmp(char* p1, char* p2) {
 	return 0;
 }
 void main() {
-	char s1[1024] = { '\0' };
-	char s2[1024] = { '\0' };
+	char s1[N];
+	char s2[N];
 	char ch;
 	printf("ÇëÊäÈës1\n");
-	for (int i = 0; i < 1023; ++i) {
+	for (int i = 0; i < N; ++i) {
 		ch = getchar();
-		if (ch == '\n') {
+		if (ch == '\n' || i == N - 1) {
+			s1[i] = '\0';
 			break;
 		}
 		s1[i] = ch;
 	}
 	printf("ÇëÊäÈës2\n");
-	for (int i = 0; i < 1023; ++i) {
+	for (int i = 0; i < N; ++i) {
 		ch = getchar();
-		if (ch == '\n') {
+		if (ch == '\n' || i == N - 1) {
+			s2[i] = '\0';
 			break;
 		}
 		s2[i] = ch;
