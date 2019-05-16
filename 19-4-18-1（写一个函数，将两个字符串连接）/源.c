35e56b385e17b78aea90d36ac7197504e3bf7d64
YYaiHH <1903870154@qq.com>
2019-05-16 15:44:05 +0800

    提交代码

--- 19-4-18-1（写一个函数，将两个字符串连接）/源.c
+++ 19-4-18-1（写一个函数，将两个字符串连接）/源.c
@@ -1,22 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-char* Strcat(char* str1, char* str2 ) {
-	int i;
-	static char str[1024];
-	for (i = 0; i < (int)strlen(str1); ++i) {
-		str[i] = str1[i];
+char* Strcat(char* str1, const char* str2 ) {
+	int n = strlen(str1);
+	for (int i = n; i < n + (int)strlen(str2); ++i) {
+		str1[i] = str2[i - n];
 	}
-	for (int j = i; j < (int)strlen(str1) + (int)strlen(str2); ++j) {
-		str[j] = str2[j - i];
-	}
-	return str;
+	return str1;
 }
 void main() {
-	char str1[] = "abcd";
-	char str2[] = "efgh";
+	char str1[256] = "abcd";
+	char str2[256] = "efgh";
 	printf("字符串1:%s\n", str1);
 	printf("字符串2:%s\n", str2);
-	printf("连接后字符串:%s\n", Strcat(str1, str2));
+	Strcat(str1, str2);
+	printf("将str2连接到str1后str1为:%s\n", str1);
 	system("pause");
 }
