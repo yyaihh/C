cd55bde76c79475a3f2beff53b8a420d080b75ad
YYaiHH <1903870154@qq.com>
2019-05-16 15:48:15 +0800

    提交代码

--- 19-5-12-4(实现strcpy)/19-5-12-4(实现strcpy)/源.c
+++ 19-5-12-4(实现strcpy)/19-5-12-4(实现strcpy)/源.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void Strcpy(char* str2, char* str1, int L) {//L为str2的长度为了防止越界问题
+void Strcpy(char* str2, const char* str1, int L) {//L为str2的长度为了防止越界问题
 	for (int i = 0; *str2 != '\0'; ++i, ++str1, ++str2) {
 		if (i == L - 1) {
 			*str2 = '\0';
@@ -17,7 +17,6 @@ void main() {
 	char str3[12];
 	char str4[20];
 	char str5[15];
-	printf("str1为:\n%s\n", str1);
 	Strcpy(str2, str1, sizeof(str2));
 	printf("用Strcpy函数将str1给str2赋值后str2为:\n%s\n", str2);
 	Strcpy(str3, str1, sizeof(str3));
