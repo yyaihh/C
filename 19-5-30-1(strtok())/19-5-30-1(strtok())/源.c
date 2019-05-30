12190abf842fe7d0a789f51b258329807b13be99
YYaiHH <1903870154@qq.com>
2019-05-30 23:30:12 +0800

    提交代码

---
+++ 19-5-30-1(strtok())/19-5-30-1(strtok())/源.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+char* Strtok(char* str, const char* sep) {
+	static char* p;
+	int i;
+	if (str != NULL) {
+		p = str;
+	}
+	else {
+		++p;
+	}
+	while (p) {
+		for (i = 0; *(sep + i) != 0; ++i) {
+			if (*p == *(sep + i)) {
+				break;
+			}
+		}
+		if (*(sep + i) != 0) {
+			*p = 0;
+		}
+	}
+}
+int main() {
+	char str1[] = "I am,a. ,/' student";
+	char str2[] = "I am,a. ,/' student";
+	for (char* p = Strtok(str1, ". ,/,"); p != NULL; p = Strtok(NULL, ". ,/'")) {
+		printf("%s ", p);
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
