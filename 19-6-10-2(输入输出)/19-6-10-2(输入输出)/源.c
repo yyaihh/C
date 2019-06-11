4924ed912b015e238083ac81b652ef7911bdecf6
YYaiHH <1903870154@qq.com>
2019-06-11 23:21:09 +0800

    提交代码

---
+++ 19-6-10-2(输入输出)/19-6-10-2(输入输出)/源.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int a;
+	int b;
+	b = scanf("%d", &a);
+	printf("%d\n%d\n", a, b);
+	system("pause");
+	return 0;
+}
