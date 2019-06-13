cb6cd8b70ea9b4d8df9bb3d95c75944359ce6bd4
YYaiHH <1903870154@qq.com>
2019-06-13 23:09:49 +0800

    提交代码MFC

---
+++ MFCApplication1/MFCApplication1/resource.h
@@ -0,0 +1,21 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCApplication1.rc 使用
+//
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1006
+#define IDC_EDIT3                       1007
+#define IDC_BUTTON1                     1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
