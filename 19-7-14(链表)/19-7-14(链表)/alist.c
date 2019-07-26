75422716b994d46abd9b44c8603a2533ba5ee555
YYaiHH <1903870154@qq.com>
2019-07-26 22:55:20 +0800

    提交代码

--- 19-7-14(链表)/19-7-14(链表)/alist.c
+++ 19-7-14(链表)/19-7-14(链表)/alist.c
@@ -1,1 +1,67 @@
 #include "alist.h"
+void SListInit(SList* plist) {
+	assert(plist);
+	plist->_head = NULL;
+}
+void SListDestory(SList* plist) {//销毁
+	assert(plist);
+	SListNode* tmp = plist->_head;
+	for (SListNode* cur; tmp; cur = tmp) {
+		tmp = cur->_next;
+		free(cur);
+	}
+}
+SListNode* BuySListNode(SLTDataType x) {
+
+}
+void SListPushFront(SList* plist, SLTDataType x) {//头插
+	assert(plist);
+	SListNode* cur = (SListNode*)malloc(sizeof(SListNode));
+	cur->_data = x;
+	cur->_next = plist->_head;
+	plist->_head = cur;
+}
+void SListPopFront(SList* plist) {//头删
+	assert(plist);
+	SListNode* cur = plist->_head;
+	plist->_head = cur->_next;
+	free(cur);
+	cur = NULL;
+}
+void SListPopBack(SList* plist, SLTDataType x) {//尾插
+	assert(plist);
+	SListNode* p;
+	for (p = plist->_head; p && p->_next; p = p->_next);
+	SListNode* cur = (SListNode*)malloc(sizeof(SListNode));
+	cur->_data = x;
+	p->_next = cur;
+	cur->_next = NULL;
+}
+void SListPopBack(SList* plist) {//尾删
+	assert(plist);
+	SListNode* p;
+	for (p = plist->_head; p && p->_next && p->_next->_next; p = p->_next);
+	free(p->_next);
+	p->_next = NULL;
+}
+SListNode* SListFind(SList* plist, SLTDataType x) {//查找
+	assert(plist);
+	for (SListNode* cur = plist->_head; cur; cur = cur->_next) {
+		if (cur->_data == x) {
+			return cur;
+		}
+	}
+	return NULL;
+}
+void SListInsertAfter(SListNode* pos, SLTDataType x) {// 在pos的后面进行插入
+	assert(pos);
+	SListNode* cur = (SListNode*)malloc(sizeof(SListNode));
+	cur->_data = x;
+	cur->_next = pos->_next;
+	pos->_next = cur;
+}
+// 在pos的前面进行插入
+void SListEraseAfter(SListNode* pos);
+void SListRemove(SList* plist, SLTDataType x);
+void SListPrint(SList* plist);
+void TestSList();
--- 19-7-14(链表)/19-7-14(链表)/alist.h
+++ 19-7-14(链表)/19-7-14(链表)/alist.h
@@ -2,3 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<assert.h>
+typedef int SLTDataType;
+typedef struct SListNode
+{
+	SLTDataType _data;
+	struct SListNode* _next;
+}SListNode;
+typedef struct SList
+{
+	SListNode* _head;
+}SList;
+void SListInit(SList* plist);
+void SListDestory(SList* plist);
+SListNode* BuySListNode(SLTDataType x);
+void SListPushFront(SList* plist, SLTDataType x);//头插
+void SListPopFront(SList* plist);//头删
+void SListPopBack(SList* plist, SLTDataType x);//尾插
+void SListPopBack(SList* plist);//尾删
+SListNode* SListFind(SList* plist, SLTDataType x);
+// 在pos的后面进行插入
+void SListInsertAfter(SListNode* pos, SLTDataType x);
+// 在pos的前面进行插入
+void SListEraseAfter(SListNode* pos);
+void SListRemove(SList* plist, SLTDataType x);
+void SListPrint(SList* plist);
+void TestSList();
