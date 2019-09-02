a683d647da5ad188106e1762a6de3db226860e9b
YYaiHH <1903870154@qq.com>
2019-09-02 20:37:53 +0800

    订单价格

--- C++/2019-9-1(订单)/2019-9-1(订单)/main.cpp
+++ C++/2019-9-1(订单)/2019-9-1(订单)/main.cpp
@@ -1,14 +1,21 @@
-#include"Class.h"
+#include"Orders.h"
 
 int main() {
 	int n;
 	cout << "请输入产品类型数\n";
 	cin >> n;
-	Class::input(n);//输入原价, 如需更改原价重新调用
-	Class a(n);
-	int m = a.count(n);
+	Orders::input(n);//输入原价, 如需更改原价重新调用
+	Orders a;
+	double m = a.count(n);
 	cout << "订单价格为:" << m << endl;
-	Class::m();
+	//更改原价, 产品数, 数量
+	cout << "请重新输入产品类型数\n";
+	cin >> n;
+	Orders::input(n);
+	m = a.count(n);
+	cout << "订单价格为:" << m << endl;
+
+	Orders::Free();
 	system("pause");
 	return 0;
 }
