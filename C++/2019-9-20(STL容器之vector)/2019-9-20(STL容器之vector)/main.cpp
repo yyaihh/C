2e9065d04245ac5545c8077676ba0ab1419e30a4
YYaiHH <1903870154@qq.com>
2019-09-21 10:28:28 +0800

    i

--- C++/2019-9-20(STL容器之vector)/2019-9-20(STL容器之vector)/main.cpp
+++ C++/2019-9-20(STL容器之vector)/2019-9-20(STL容器之vector)/main.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main() {
+	vector<int> arr,arr1(10);
+	vector<string> str(10);
+	for (int i = 0; i < 10; ++i) {
+		arr.push_back(i + 1);
+		cout << arr.capacity() << " ";
+	}
+	cout << endl;
+	for (auto i : arr) {
+		cout << i << ' ';
+	}
 
+	cout << endl;
+	arr.pop_back();
+	for (auto i : arr) {
+		cout << i << ' ';
+	}
+	cout << endl;
+	cout << endl;
+	system("pause");
+	return 0;
 
 }
