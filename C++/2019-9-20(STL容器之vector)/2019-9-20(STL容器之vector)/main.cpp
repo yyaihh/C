842ca6bcb0f175ea4c83d1291b92ecbb2d77341c
YYaiHH <1903870154@qq.com>
2019-09-21 23:38:03 +0800

    vector测试

--- C++/2019-9-20(STL容器之vector)/2019-9-20(STL容器之vector)/main.cpp
+++ C++/2019-9-20(STL容器之vector)/2019-9-20(STL容器之vector)/main.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 
 int main() {
-	vector<int> arr,arr1(10);
+	vector<int> arr, arr1(10, -1);
 	vector<string> str(10);
 	for (int i = 0; i < 10; ++i) {
-		arr.push_back(i + 1);
-		cout << arr.capacity() << " ";
+		arr.push_back(i + 1); 
 	}
+	//vector<int>::iterator i = arr.erase(arr.begin() + 2, arr.begin() + 4);
+	vector<int>::iterator i = arr.insert(arr.begin() + 2, arr1.begin(), arr1.end());
+	vector<int>::iterator j = arr.insert(arr.begin() + 2, 3, 10);
+	cout << *i << endl;
 	cout << endl;
 	for (auto i : arr) {
 		cout << i << ' ';
 	}
-
 	cout << endl;
 	arr.pop_back();
 	for (auto i : arr) {
@@ -23,5 +25,4 @@ int main() {
 	cout << endl;
 	system("pause");
 	return 0;
-
 }
