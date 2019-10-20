494027eb8ee7110a6f7aa6e59dcb5aa8a042d831
YYaiHH <1903870154@qq.com>
2019-10-20 13:23:27 +0800

    增加O(1)复杂度的方法

--- C++/2019-10-16(求1+2+ +n要求不能使用乘除法for,while等关键字及条件判断语句)/2019-10-16(求1+2+ +n要求不能使用乘除法for,while等关键字及条件判断语句)/main.cpp
+++ C++/2019-10-16(求1+2+ +n要求不能使用乘除法for,while等关键字及条件判断语句)/2019-10-16(求1+2+ +n要求不能使用乘除法for,while等关键字及条件判断语句)/main.cpp
@@ -6,26 +6,40 @@ using namespace std;
 //1.ตÝน้
 //2.sum=n(n+1)/2
 using namespace std;
+#if 0
 class Solution {
-	int sum;
 public:
 	int Sum_Solution(int n) {
 		int _sum = n;
 		n > 0 && (_sum += Sum_Solution(n - 1));
 		return _sum;
 	}
-	Solution():sum(0) {
-
-	}
-	/*int Sum_Solution(int n) {
-		n > 0 && Sum_Solution(n - 1);
-		sum += n;
-		return sum;
-	}*/
 	/*int Sum_Solution(int n) {
 		return ((int)pow(n, 2) + n) >> 1;
 	}*/
 };
+#else
+class _Solution {
+public:
+	static int tmp;
+	static int sum;
+	_Solution() {
+		++tmp;
+		sum += tmp;
+	}
+};
+int _Solution::sum = 0;
+int _Solution::tmp = 0;
+class Solution {
+public:
+	int Sum_Solution(int n) {
+		_Solution::tmp = 0;
+		_Solution::sum = 0;
+		_Solution* p = new _Solution[n];
+		return _Solution::sum;
+	}
+};
+#endif
 int main() {
 	Solution p;
 	cout << p.Sum_Solution(3) << endl;
