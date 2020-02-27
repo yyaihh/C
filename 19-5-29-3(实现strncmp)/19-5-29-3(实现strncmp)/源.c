36e8efaad81e727c585bfe850bf35b5d4e6254d7
YYaiHH <1903870154@qq.com>
2020-02-27 23:59:42 +0800

    提交

--- 19-5-29-3(实现strncmp)/19-5-29-3(实现strncmp)/源.c
+++ 19-5-29-3(实现strncmp)/19-5-29-3(实现strncmp)/源.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int Strncmp(const char* str1, const char* str2 ,int size) {
 	for (int i = 0; i < size; ++i) {
 		if (*(str1 + i) > *(str2 + i)) {
@@ -22,7 +23,13 @@ int main() {
 	printf("%d\n", Strncmp(str1, str2, 100));
 	printf("%d\n", Strncmp(str2, str1, 4));
 	printf("%d\n", Strncmp(str2, str1, 5));
-	printf("%d\n", Strncmp(str2, str1, 100));
+	printf("%d\n\n", Strncmp(str2, str1, 100));
+	printf("%d\n", strncmp(str1, str2, 4));
+	printf("%d\n", strncmp(str1, str2, 5));
+	printf("%d\n", strncmp(str1, str2, 100));
+	printf("%d\n", strncmp(str2, str1, 4));
+	printf("%d\n", strncmp(str2, str1, 5));
+	printf("%d\n", strncmp(str2, str1, 100));
 	system("pause");
 	return 0;
 }
